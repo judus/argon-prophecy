0cf666afdfb548fba127ee41be1ec4a8d3c6c1c7
Julien Duseyau <julien.duseyau@gmail.com>
2025-04-07 22:29:16 +0200

    Initial setup

--- tests/applications/app-skeleton/app/src/Http/Controllers/HomeController.php
+++ tests/applications/app-skeleton/app/src/Http/Controllers/HomeController.php
@@ -6,20 +6,41 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\TestClassInterface;
 use JsonSerializable;
+use Psr\Http\Message\ServerRequestInterface;
 use RuntimeException;
 
 readonly class HomeController
 {
+    public function __construct(private ServerRequestInterface $request)
+    {
+    }
+
     public function index(): JsonSerializable
     {
-        return new class implements JsonSerializable {
+        $request = $this->request;
+
+        return new class($request) implements JsonSerializable {
+            public function __construct(private ServerRequestInterface $request) {}
+
             public function jsonSerialize(): array
             {
+                $uri = $this->request->getUri();
+                $baseUrl = rtrim($uri->getScheme() . '://' . $uri->getHost() . ':' . (string )$uri->getPort(), '/');
+
                 return [
                     'name' => 'Argon Prophecy',
-                    'version' => '1.0.0-beta.6b',
-                    'description' => 'A strict, DI-first and PSR-compliant PHP runtime for law-abiding Prophets.',
+                    'version' => '0.0.1-dev',
+                    'container' => '1.0.0-beta.7',
+                    'description' => 'A strict, DI-First and PSR-compliant PHP runtime for law-abiding Prophets.',
                     'current_time' => date('Y-m-d H:i:s'),
+                    'routes' => [
+                        'root' => "$baseUrl/",
+                        'di only' => "$baseUrl/demo/injected",
+                        'di + param' => "$baseUrl/demo/injected/123",
+                        'two params' => "$baseUrl/demo/params/456/important",
+                        'html/plain' => "$baseUrl/demo/plain",
+                        'throws exception' => "$baseUrl/demo/error",
+                    ],
                 ];
             }
         };
